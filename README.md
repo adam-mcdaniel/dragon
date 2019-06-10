e601d2606c60c35e95440e788d4f8883c57b4369
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-06-10 09:06:21 -0400

    Added readme info

---
+++ README.md
@@ -0,0 +1,44 @@
+# dragon
+
+A statically compiled, dynamically typed programming language.
+
+## Getting Started
+
+Install dependencies.
+
+```bash
+# Ubuntu
+sudo apt-get install make
+sudo apt-get install cmake
+
+sudo apt-get install python3-dev
+sudo apt-get install python3-pip
+python3 -m pip install lark-parser
+```
+
+Clone the repo.
+
+```bash
+git clone "https://github.com/adam-mcdaniel/dragon
+
+cd dragon
+```
+
+Build dragon.
+
+```bash
+mkdir build
+cd build
+
+cmake ..
+```
+
+Compile a script.
+
+```bash
+# in build directory
+cat ../examples/script.dn | python ../compiler.py > ../src/main.cpp;
+
+make
+./dragon
+```
--- src/main.cpp
+++ src/main.cpp
@@ -6,33 +6,29 @@
 int main()
 {
 	auto dragon = Machine();
-	dragon.push(Object::Fn([](Machine &dragon) { dragon.push(Object::Map()); }));
+	dragon.push(Object::Fn([](Machine& dragon) { dragon.push(Object::Map()); }));
 	dragon.push(Object::String("dict"));
 	dragon.store();
 
-	dragon.push(Object::Fn([](Machine &dragon) { dragon.push(Object::List({})); }));
+	dragon.push(Object::Fn([](Machine& dragon) { dragon.push(Object::List({})); }));
 	dragon.push(Object::String("list"));
 	dragon.store();
 
-	dragon.push(Object::Fn([](Machine &dragon) {
-		dragon.push(Object::String("dict"));
-		dragon.load();
-		dragon.call();
-		dragon.push(Object::String("self"));
-		dragon.store();
+	dragon.push(Object::Fn([](Machine& dragon) { 		dragon.push(Object::String("dict"));
+	dragon.load();
+dragon.call();
+	dragon.push(Object::String("self"));
+	dragon.store();
 
 		dragon.push(Object::String("dict"));
-		dragon.load();
-		dragon.call();
+	dragon.load();
+dragon.call();
 		dragon.push(Object::String("self"));
-		dragon.load();
-		;
-		dragon.push(Object::String("open"));
-		;
-		dragon.index();
-		dragon.assign();
+	dragon.load();
+;dragon.push(Object::String("open"));;dragon.index();
+	dragon.assign();
 
-		dragon.push(Object::Fn([](Machine &dragon) { 	dragon.push(Object::String("s"));
+	dragon.push(Object::Fn([](Machine& dragon) { 	dragon.push(Object::String("s"));
 	dragon.store();
 
 	dragon.push(Object::String("name"));
@@ -42,42 +38,27 @@ dragon.push(Object::Fn([](Machine& dragon) { 	dragon.push(Object::String("name")
 	dragon.load();
 dragon.push(Object::String("Opening file: "));std::cout << (*dragon.pop()).format() << (*dragon.pop()).format()  << std::endl; }));dragon.call(); }));
 		dragon.push(Object::String("self"));
-		dragon.load();
-		;
-		dragon.push(Object::String("open"));
-		;
-		dragon.index();
-		;
-		dragon.push(Object::String("fn"));
-		;
-		dragon.index();
-		dragon.assign();
+	dragon.load();
+;dragon.push(Object::String("open"));;dragon.index();;dragon.push(Object::String("fn"));;dragon.index();
+	dragon.assign();
 
-		dragon.push(Object::String("self"));
-		dragon.load();
-	}));
+	dragon.push(Object::String("self"));
+	dragon.load();
+ }));
 	dragon.push(Object::String("File"));
 	dragon.store();
 
-	dragon.push(Object::String("File"));
+		dragon.push(Object::String("File"));
 	dragon.load();
-	dragon.call();
+dragon.call();
 	dragon.push(Object::String("f"));
 	dragon.store();
 
-	dragon.push(Object::String("my_file"));
-	dragon.push(Object::String("f"));
+dragon.push(Object::String("my_file")); 	dragon.push(Object::String("f"));
 	dragon.load();
-	dragon.push(Object::String("f"));
+ 	dragon.push(Object::String("f"));
 	dragon.load();
-	;
-	dragon.push(Object::String("open"));
-	;
-	dragon.index();
-	;
-	dragon.push(Object::String("fn"));
-	;
-	dragon.index();
-	dragon.call();
-	std::cout << dragon.format() << std::endl;
+;dragon.push(Object::String("open"));;dragon.index();;dragon.push(Object::String("fn"));;dragon.index();dragon.call();
+    std::cout << dragon.format() << std::endl;
 }
+
