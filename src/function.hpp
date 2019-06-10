50b0322bc6521b441f34b0797535d7ae015d94fc
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-06-10 08:58:28 -0400

    Implemented basic CMake build script

--- src/function.hpp
+++ src/function.hpp
@@ -1,19 +1,23 @@
 #pragma once
 #include <functional>
 
-template<typename I, typename O>
-class Function {
+template <typename I, typename O>
+class Function
+{
 public:
     Function() {}
 
-    template<class T>
-    Function(T t) {
-        this->f = std::function(t);
+    template <class T>
+    Function(T t)
+    {
+        this->f = std::function<O(I)>(t);
     }
 
-    O operator ()(I i) {
+    O operator()(I i)
+    {
         return this->f(i);
     }
+
 private:
     std::function<O(I)> f;
 };
